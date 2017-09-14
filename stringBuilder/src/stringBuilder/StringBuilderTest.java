3786558f3db36017878294201c0e27fb98f0fcb1
renuka <renutarse1@gmail.com>
2017-09-14 17:16:10 +0530

    stringbuilder commit

---
+++ stringBuilder/src/stringBuilder/StringBuilderTest.java
@@ -0,0 +1,12 @@
+package stringBuilder;
+
+public class StringBuilderTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         StringBuilder builder=new StringBuilder("hello");
+         builder.append("Renu");
+         System.out.println(builder);
+	}
+
+}
